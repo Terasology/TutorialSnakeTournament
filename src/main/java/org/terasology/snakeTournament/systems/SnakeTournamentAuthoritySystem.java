c6fc53307c5c713c5ab54427ce246932819131fd
DarkWeird <foxflameless@gmail.com>
2021-08-30 11:29:28 +0300

    chore(tutorial): migrate tutorial modules with engine package movement

--- src/main/java/org/terasology/snakeTournament/systems/SnakeTournamentAuthoritySystem.java
+++ src/main/java/org/terasology/snakeTournament/systems/SnakeTournamentAuthoritySystem.java
@@ -15,12 +15,12 @@
  */
 package org.terasology.snakeTournament.systems;
 
-import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.event.ReceiveEvent;
-import org.terasology.entitySystem.systems.BaseComponentSystem;
-import org.terasology.entitySystem.systems.RegisterMode;
-import org.terasology.entitySystem.systems.RegisterSystem;
-import org.terasology.logic.health.event.DoDamageEvent;
+import org.terasology.engine.entitySystem.entity.EntityRef;
+import org.terasology.engine.entitySystem.event.ReceiveEvent;
+import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
+import org.terasology.engine.entitySystem.systems.RegisterMode;
+import org.terasology.engine.entitySystem.systems.RegisterSystem;
+import org.terasology.engine.logic.health.event.DoDamageEvent;
 import org.terasology.snakeTournament.events.CharacterTrappedEvent;
 
 @RegisterSystem(RegisterMode.AUTHORITY)
